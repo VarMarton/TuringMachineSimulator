f2204d7d171b7a558cd1c3bf13599058d843d893
VarMarton <vargamarci666@gmail.com>
2021-02-10 16:30:20 +0100

    TapeSettings generation finished

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -3,19 +3,31 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private TapeSettingsController tapeSettingsController;
+
+    @FXML
+    private GridPane tapesSettingContainer;
+    @FXML
+    private Button newTapeBtn;
+    @FXML
+    private Button deleteTapeBtn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        tapeSettingsController = new TapeSettingsController(tapesSettingContainer, newTapeBtn, deleteTapeBtn);
+        tapeSettingsController.addNewTapeSetting();
+
+        newTapeBtn.setOnMouseClicked(event -> tapeSettingsController.addNewTapeSetting());
+        deleteTapeBtn.setOnMouseClicked(event -> tapeSettingsController.removeTapeSetting());
     }
 }
---
+++ src/main/java/controller/TapeSettingsController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+
+public class TapeSettingsController {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final String[] TAPE_SETTING_NAMES = {"A", "B", "C", "D", "E"};
+
+    private GridPane root;
+    private ArrayList<OneTapeSettingController> tapeSettingControllers;
+    private Button newTapeBtn;
+    private Button deleteTapeBtn;
+
+    public TapeSettingsController(GridPane root, Button newTapeBtn, Button deleteTapeBtn) {
+        this.root = root;
+        this.newTapeBtn = newTapeBtn;
+        this.tapeSettingControllers = new ArrayList<>();
+        this.deleteTapeBtn = deleteTapeBtn;
+    }
+
+    public void addNewTapeSetting() {
+        OneTapeSettingController tapeSetting = new OneTapeSettingController(this.root, tapeSettingControllers.size(), TAPE_SETTING_NAMES[tapeSettingControllers.size()]);
+        tapeSettingControllers.add(tapeSetting);
+        if (tapeSettingControllers.size() == TAPE_SETTING_NAMES.length) {
+            this.newTapeBtn.setDisable(true);
+        }
+        if(tapeSettingControllers.size() > 1){
+            this.deleteTapeBtn.setDisable(false);
+        }
+    }
+
+    public void removeTapeSetting() {
+        this.root.getChildren().remove(tapeSettingControllers.size() - 1);
+        tapeSettingControllers.remove(tapeSettingControllers.size() - 1);
+        if (tapeSettingControllers.size() == 1) {
+            this.deleteTapeBtn.setDisable(true);
+        }
+        if (tapeSettingControllers.size() < TAPE_SETTING_NAMES.length) {
+            this.newTapeBtn.setDisable(false);
+        }
+    }
+
+}
