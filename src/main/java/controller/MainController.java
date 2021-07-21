32e31755749af708892797b632f8da988dd01fd4
VarMarton <vargamarci666@gmail.com>
2021-07-22 01:30:02 +0200

    Finished static ui with changeable layout

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,48 +1,88 @@
 package controller;
 
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import view.Tape;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private boolean areCentralListenersSet = false;
+    private boolean isLayoutChanged = false;
+
     private TapeSettingsController tapeSettingsController;
 
     @FXML
-    private BorderPane mainPane;
+    private Double SECTION_MARGIN;
     @FXML
-    private GridPane tapesSettingContainer;
+    private Double FIRST_ROW_DEFAULT_HEIGHT;
+
     @FXML
-    private Button newTapeBtn;
+    private GridPane center;
     @FXML
-    private Button deleteTapeBtn;
+    private BorderPane ruleSection;
     @FXML
-    private Button initialiseBtn;
+    private GridPane tapeSection;
     @FXML
-    private GridPane tapesContainer;
+    private Button makeRuleSectionBigBtn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        tapeSettingsController = new TapeSettingsController(tapesSettingContainer, newTapeBtn, deleteTapeBtn);
-        tapeSettingsController.addNewTapeSetting();
+        this.makeRuleSectionBigBtn.setOnMouseClicked(event -> {
+            this.makeRuleSectionBigEvent();
+        });
 
+        /*
         newTapeBtn.setOnMouseClicked(event -> tapeSettingsController.addNewTapeSetting());
         deleteTapeBtn.setOnMouseClicked(event -> tapeSettingsController.removeTapeSetting());
+        */
+    }
 
+    private void setCentralListener(){
+        this.center.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            if (this.isLayoutChanged) {
+                this.restoreLayout();
+            }
+        });
 
+        this.center.widthProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            if (this.isLayoutChanged) {
+                this.restoreLayout();
+            }
+        });
+        this.areCentralListenersSet = true;
+    }
+
+    private void makeRuleSectionBigEvent(){
+        if (!this.isLayoutChanged) {
+            this.makeRuleSectionBig();
+        } else {
+            this.restoreLayout();
+        }
+    }
+
+    private void makeRuleSectionBig() {
+        this.isLayoutChanged = true;
+        this.makeRuleSectionBigBtn.setText("] [");
+        this.tapeSection.setMaxWidth(this.center.getWidth() - this.ruleSection.getWidth() - this.SECTION_MARGIN);
+        this.ruleSection.setMinHeight(this.center.getHeight());
+        this.tapeSection.setMaxHeight(this.center.getHeight() - this.SECTION_MARGIN - this.FIRST_ROW_DEFAULT_HEIGHT);
+    }
+
+    private void restoreLayout() {
+        this.makeRuleSectionBigBtn.setText("[ ]");
+        this.ruleSection.setMinHeight(Region.USE_COMPUTED_SIZE);
+        this.tapeSection.setMaxWidth(Region.USE_COMPUTED_SIZE);
+        this.tapeSection.setMaxHeight(Region.USE_COMPUTED_SIZE);
+        this.isLayoutChanged = false;
     }
 }
